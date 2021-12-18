b0b932c59725a32a8891f4dfea550336507395b7
SCxoneZ <89004475+SCxoneZ@users.noreply.github.com>
2021-12-18 13:58:04 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,45 +1,22 @@
-const input = document.querySelector('.input-todo');
-const btn = document.querySelector('.btn-todo');
-const contTodo = document.querySelector('.container-todo');
-let allTodo = document.querySelectorAll('.todo');
+const price = document.querySelector('#price');
+const sales = document.querySelector('#total-sales');
+const exRevenue = document.querySelector('#revenue');
 
-btn.addEventListener('click', () => {
-  const inputValue = input.value;
-  let newEl = document.createElement('div');
-  newEl.classList.add('todo');
-  newEl.innerHTML = inputValue;
-  newEl.style.backgroundColor = getRandHex();
-  contTodo.appendChild(newEl);
-  input.value = '';
-  allTodo = document.querySelectorAll('.todo');
-  allTodo.forEach((el) => {
-    el.addEventListener('click', () => {
-      el.remove();
-    });
-  });
-});
+const daysSpan = document.querySelector('.days');
 
-function getRandHex(){
-  const alphabets = 'ABCDEF';
-  let hexCode = '#';
-  
-  for(let i = 0; i < 6; i++){
-    
-    let ToF;
-    const random1 = Math.random();
-    
-    if(random1 < 0.5){
-      ToF = true;
-    }else{
-      ToF = false;
+
+setInterval(() => {
+  if(price.value && sales.value && exRevenue.value !== ''){
+    if(parseInt(price.value) && parseInt(sales.value) && parseInt(exRevenue) !== NaN){
+      
+      let priceValue = parseInt(price.value);
+      let salesValue = parseInt(sales.value);
+      let revenueValue = parseInt(exRevenue.value);
+      
+      daysSpan.innerHTML = revenueValue/salesValue;
+      
+      
+      
     }
-    
-    if(!ToF){
-      hexCode += alphabets[Math.floor(Math.random()*alphabets.length)]
-    }else{
-      hexCode += Math.floor(Math.random()*6);
-    }
-    
   }
-  return hexCode;
-}
+}, 100);
